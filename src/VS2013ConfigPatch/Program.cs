0a10938de47496aada4ec8af93660009319b4dbd
Amadeus Wieczorek <git@amadeusw.com>
2015-05-31 17:41:39 -0400

    Creates an command line utility
    Utility that patches VS2013's devenv.exe.config to use MSBuild 2015

---
+++ src/VS2013ConfigPatch/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace VS2013ConfigPatch
+{
+    class Program
+    {
+        const string LOCATION_WITHIN_PROGRAM_FILES = @"Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe.config";
+        const string ASSEMBLY_BASE = @"Microsoft.Build";
+        const string ASSEMBLY1 = @"Microsoft.Build";
+        const string ASSEMBLY2 = @"Microsoft.Build.Engine";
+        const string ASSEMBLY3 = @"Microsoft.Build.Framework";
+        const string OLD_VERSION_PATCHED = @"2.0.0.0 - 12.0.0.0";
+        const string OLD_VERSION_UNDO = @"2.0.0.0 - 4.0.0.0";
+        const string NEW_VERSION_PATCHED = @"14.0.0.0";
+        const string NEW_VERSION_UNDO = @"12.0.0.0";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var targetFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), LOCATION_WITHIN_PROGRAM_FILES);
+                if (args?.FirstOrDefault() == "-u")
+                {
+                    BackUpFile(targetFileName, undo: true);
+                    ModifyXml(targetFileName, undo: true);
+                }
+                else
+                {
+                    BackUpFile(targetFileName);
+                    ModifyXml(targetFileName);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("\nRun this program with administrative privileges.");
+            }
+            catch (FileNotFoundException fe)
+            {
+                Console.WriteLine($"\nCouldn't find file {fe.FileName}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nExecution aborted: " + e.ToString());
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void BackUpFile(string targetFileName, bool undo = false)
+        {
+            var backupFileName = undo ? targetFileName + ".undoBackup" : targetFileName + ".backup";
+            Console.WriteLine($"Backing up {targetFileName} into {backupFileName}...");
+            try
+            {
+                File.Copy(targetFileName, backupFileName);
+            }
+            catch (IOException)
+            {
+                // destination file already exists. We don't want to do any work without creating a backup,
+                // but we also don't want to overwrite an existing backup.
+                Console.WriteLine($"Backup file {backupFileName} already exists. Save it elsewhere, remove it manually, then re-run the script.");
+                throw;
+            }
+            Console.WriteLine($"Backup successful.");
+        }
+
+        public static void ModifyXml(string path, bool undo = false)
+        {
+            bool patchedAssembly1 = false;
+            bool patchedAssembly2 = false;
+            bool patchedAssembly3 = false;
+
+            XDocument doc = XDocument.Load(path);
+            XNamespace ns = "urn:schemas-microsoft-com:asm.v1";
+
+            var level1 = doc.Descendants("runtime").First();
+            var level2 = level1.Descendants(ns + "assemblyBinding").First();
+            var assemblies = level2.Descendants(ns + "dependentAssembly");
+            foreach (var assembly in assemblies)
+            {
+                var identity = assembly.Descendants(ns + "assemblyIdentity");
+                var assemblyName = identity.Attributes().Where(attr => attr.Name == "name").First().Value;
+                if (assemblyName.StartsWith(ASSEMBLY_BASE))
+                {
+                    var redirect = assembly.Descendants(ns + "bindingRedirect").First();
+                    redirect.SetAttributeValue("oldVersion", undo ? OLD_VERSION_UNDO : OLD_VERSION_PATCHED);
+                    redirect.SetAttributeValue("newVersion", undo ? NEW_VERSION_UNDO : NEW_VERSION_PATCHED);
+                    patchedAssembly1 |= assemblyName == ASSEMBLY1;
+                    patchedAssembly2 |= assemblyName == ASSEMBLY2;
+                    patchedAssembly3 |= assemblyName == ASSEMBLY3;
+                }
+                if (patchedAssembly1 && patchedAssembly2 && patchedAssembly3)
+                {
+                    // We're done early
+                    break;
+                }
+            }
+            if (patchedAssembly1 && patchedAssembly2 && patchedAssembly3)
+            {
+                // Save the xml
+                doc.Save(path);
+                Console.WriteLine("Patch successful :)");
+            }
+            else
+            {
+                if (!patchedAssembly1)
+                    Console.WriteLine("Unable to patch {ASSEMBLY1}");
+                if (!patchedAssembly2)
+                    Console.WriteLine("Unable to patch {ASSEMBLY2}");
+                if (!patchedAssembly3)
+                    Console.WriteLine("Unable to patch {ASSEMBLY3}");
+                throw new ApplicationException("Unable to patch the configuration file.");
+            }
+        }
+    }
+}
---
+++ src/VS2013ConfigPatch/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VS2013ConfigPatch")]
+[assembly: AssemblyDescription("Patches VS2013's devenv.exe.config to use MSBuild 2015")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Code Connect")]
+[assembly: AssemblyProduct("VS2013ConfigPatch")]
+[assembly: AssemblyCopyright("Copyright © Code Connect 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c6e2657-c72f-4e38-a9ba-6fd525f46cb1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
